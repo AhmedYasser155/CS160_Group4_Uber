3cbe9563cad088f265d0be9db5c95cd2decc48d1
Hang Ho <hang.ho@sjsu.edu>
2022-03-06 12:46:48 -0800

    draw path line from pickup to drop off location

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import tw from "tailwind-styled-components"
 import mapboxgl from '!mapbox-gl'
 
@@ -8,14 +8,17 @@ mapboxgl.accessToken = "pk.eyJ1IjoiaGFuZy1obyIsImEiOiJjbDA2M3F6bm4xcW05M2RvZHhpe
 
 const Map = (props) => {
 
+  const [coordinates, setCoordinates] = useState()
+
   useEffect( () => {
       const map = new mapboxgl.Map({
         container: "map",
         style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
         center: [-99.29011, 39.39172],
         zoom: 3,
-    
       })
+      map.addControl(nav)
+      
       if (props.pickupCoordinates) {
         addToMap(map, props.pickupCoordinates)
       }
@@ -27,6 +30,40 @@ const Map = (props) => {
           props.pickupCoordinates,
           props.dropoffCoordinates
         ], {padding: 60})
+
+        fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${props.pickupCoordinates[0]},${props.pickupCoordinates[1]};${props.dropoffCoordinates[0]},${props.dropoffCoordinates[1]}?geometries=geojson&steps=true&access_token=pk.eyJ1IjoiaGFuZy1obyIsImEiOiJjbDA2M3F6bm4xcW05M2RvZHhpeDFsZTVvIn0.Ot8ZrqGcvLYWRLzyXtkUdA`)
+        .then(res => res.json())
+        .then(data => {
+          setCoordinates(data.routes[0].geometry.coordinates)
+          console.log(coordinates)
+
+          map.on('load', () => {
+            map.addSource('route', {
+              'type': 'geojson',
+              'data': {
+                'type': 'Feature',
+                'properties': {},
+                'geometry': {
+                  'type': 'LineString',
+                  'coordinates': coordinates
+                }
+              }
+            });
+            map.addLayer({
+              'id': 'route',
+              'type': 'line',
+              'source': 'route',
+              'layout': {
+                'line-join': 'round',
+                'line-cap': 'round'
+              },
+            'paint': {
+              'line-color': '#313631',
+              'line-width': 8
+            }
+            });
+          });
+        }).catch((e) => console.log(e))
       }
   }, [props.pickupCoordinates, props.dropoffCoordinates])
 
@@ -35,6 +72,8 @@ const Map = (props) => {
       .setLngLat(coordinates)
       .addTo(map)
   }
+  
+  const nav = new mapboxgl.NavigationControl()
 
   return (
     <Wrapper id="map"></Wrapper>
