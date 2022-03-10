b04b3b5ee075870e07976ffe6c99bf7d5c6907d1
Sunny Chen <sunny.m.chen3@gmail.com>
2022-03-09 23:49:09 -0800

    added toggle for sign up page to differentiate between driver and rider signup

--- pages/signup.js
+++ pages/signup.js
@@ -3,16 +3,17 @@ import { useState } from 'react'
 import tw from "tailwind-styled-components"
 import { BackHomeButton } from '../components/BackHomeButton'
 import Link from 'next/Link'
+import { DriverSignUp } from '../components/DriverSignUp'
+import { RiderSignUp } from '../components/RiderSignUp'
+
 
 
 
 const SignUp = () => {
     
-    const [firstName, setFirstName] = useState()
-    const [lastName, setLastName] = useState()
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-    const [phoneNumber, setPhoneNumber] = useState()
+
+    const [select, setSelect] = useState("rider")
+    const [isToggled, setIsToggled] = useState(false);
     
 
     return(
@@ -25,37 +26,51 @@ const SignUp = () => {
                      <UberLogo src="https://download.logo.wine/logo/Uber/Uber-Logo.wine.png"/> 
                 </Header>
 
-                <InputContainer>
-                    <InputBoxes>
-                    <Input
-                        placeholder = "First Name"
-                        value = {firstName}
-                        onChange={(e) => {setFirstName(e.target.value)}}
-                    />
-                    <Input
-                        placeholder = "Last Name"
-                        value = {lastName}
-                        onChange={(e) => {setLastName(e.target.value)}}
-                    />
-                     <Input
-                        placeholder = "Email"
-                        value = {email}
-                        onChange={(e) => {setEmail(e.target.value)}}
-                    />
-                    <Input
-                        placeholder = "Password"
-                        value = {password}
-                        onChange={(e) => {setPassword(e.target.value)}}
-                    />
-                    <Input
-                        placeholder = "Phone Number"
-                        value = {phoneNumber}
-                        onChange={(e) => {setPhoneNumber(e.target.value)}}
-                    />
-                    </InputBoxes>
-
-                </InputContainer>
+                <RadioButtons>
+                    
+               
+                        
+                        <Input
+                                type = "radio"
+                                label = "rider"
+                                value = "rider"
+                                checked = {select == "rider"}
+                                onChange={(e) => {setSelect(e.target.value),setIsToggled(!isToggled)}}
+                                
+    
+                            />
+                           <RadioButtonLabel>
+                               
+                                I am a Rider
+    
+                           </RadioButtonLabel>
         
+                             <Input
+                                type = "radio"
+                                label = "driver"
+                                value = "driver"
+                                checked = {select == "driver"}
+                                onChange={(e) => {setSelect(e.target.value, setIsToggled(!isToggled))}}
+                                
+    
+                            />
+                           <RadioButtonLabel>
+                               
+                                I am a Driver
+    
+                           </RadioButtonLabel>
+    
+                        
+    
+                    </RadioButtons>
+
+                    {isToggled ? <DriverSignUp/> : <RiderSignUp/>}
+
+
+
+
+
+
                 <Link href="/">
                     <ActionButton>
                      Sign up
@@ -83,12 +98,6 @@ const UberLogo = tw.img`
     h-28 w-auto object-contain self-start
 `
 
-const InputContainer = tw.div`
-    bg-white flex items-center px-4 mb-2
-`
-const InputBoxes = tw.div`
-    flex flex-col flex-1
-`
 const Input = tw.input`
     h-10 bg-gray-200 my-2 rounded-2 p-2 outline-none border-none
 `
@@ -98,4 +107,11 @@ const ActionButton = tw.div`
 hover:scale-105 transition text-xl
 
 `
+const RadioButtons = tw.div`
+    bg-white flex flex-row px-4
+`
+const RadioButtonLabel = tw.label`
+    flex flex-row items-center ml-1 mr-4
+`
+
 
