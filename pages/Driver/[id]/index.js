83e79e55c4a62d528a065015187bcb9839bf8283
andrewtedi <andrew.tedijanto@gmail.com>
2022-05-04 20:04:44 -0700

    rebasing

--- pages/Driver/[id]/index.js
+++ pages/Driver/[id]/index.js
@@ -5,20 +5,30 @@ import tw from "tailwind-styled-components"
 import Map from '../../../components/Map'
 import Link from 'next/Link'
 import { io } from 'socket.io-client';
+import {getUser} from '../../../APIFunctions/DbFunctions'
+import  {useRouter} from 'next/router'
 
 const socket = io("http://localhost:3001");
 
-export default function Home() {
+export default function Home({name}) {
+
+  const router = useRouter()
+  const id = router.query.id
+
   return (
     <Wrapper>
       <Map/>
       <ActionItems>
         <Header>
           <UberLogo src="https://download.logo.wine/logo/Uber/Uber-Logo.wine.png"></UberLogo>
+          <Link href={`/Driver/${id}/driverprofile`}> 
           <Profile>
-            <Name>Name</Name>
+            <Name>
+              {name}
+            </Name>
             <UserImage src="https://png.pngtree.com/png-vector/20190909/ourmid/pngtree-outline-user-icon-png-image_1727916.jpg"/>
           </Profile>
+          </Link>
         </Header>
 {/* This infomation will be retrieved from database */}
         <InforSection>
@@ -35,6 +45,17 @@ export default function Home() {
     </Wrapper>
   )
 }
+export async function getServerSideProps({params})
+{
+     const user = await getUser(params.id);
+    const name = params.id
+     return{
+         props:{
+            name,
+         },
+     };
+
+    }
 
 const Label = tw.div`
     text-2xl font-semibold
@@ -63,7 +84,7 @@ const Profile = tw.div`
   flex items-center
 `
 
-const Name = tw.div`
+const Name = tw.p`
   mr-4 w-20 text-sm
 `
 
